4d2bf965b46c4c5194a3d5a4c0ac6bddda02ce08
Jesus Mejia <jesusmejded@gmail.com>
2020-10-13 09:13:45 -0500

    importing componenets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './components/user/user.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { SponsorsComponent } from './components/sponsors/sponsors.component';
+import { DescriptionComponent } from './components/description/description.component';
+import { OrganizersComponent } from './components/organizers/organizers.component';
+import { SubmissiondetailsComponent } from './components/submissiondetails/submissiondetails.component';
+import { VenueComponent } from './components/venue/venue.component';
+import { CommiteeComponent } from './components/commitee/commitee.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { EventComponent } from './components/event/event.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    BannerComponent,
+    SponsorsComponent,
+    DescriptionComponent,
+    OrganizersComponent,
+    SubmissiondetailsComponent,
+    VenueComponent,
+    CommiteeComponent,
+    FooterComponent,
+    EventComponent,
   ],
   imports: [
+    HttpClientModule,
     BrowserModule
   ],
   providers: [],
